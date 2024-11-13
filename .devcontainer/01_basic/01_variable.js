ab6c29054f62c25caab69ba94401d5efa2cb8b95
aaref12 <111276585+aaref12@users.noreply.github.com>
2024-11-13 07:14:32 +0000

    variable and datatypes

---
+++ .devcontainer/01_basic/01_variable.js
@@ -0,0 +1,7 @@
+const accountId=1234567;
+let accountName="shaikh aaref hamza";
+var accountType="seving"
+accountAddress="Nanded"
+
+
+console.table([accountId,accountName,accountType,accountAddress])

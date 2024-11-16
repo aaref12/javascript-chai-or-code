4e8ea3c2001b10c5b01d5b8b695de452cedcc128
aaref12 <111276585+aaref12@users.noreply.github.com>
2024-11-16 08:10:52 +0000

    number_and_math

---
+++ .devcontainer/01_basic/number_and_math.js
@@ -0,0 +1,23 @@
+ //const score=100
+ //const blance=new Number(100)
+ //const number=12.300000
+ //console.log(blance)
+ //console.log(score.toString())
+
+ //console.log(number.toFixed(2))
+ //console.log(number.toLocaleString())
+ //console.log(number.toPrecision(5))
+
+//***********************Math******************************
+
+
+//console.log(Math)
+//console.log(Math.abs(-4))
+//console.log(Math.round(4.2))
+//console.log(Math.ceil(4.1))
+//console.log(Math.floor(4.4))
+const min=10;
+const max=20;
+console.log(Math.random())
+console.log((Math.random()*10)+1)
+console.log(Math.floor(Math.random()*(max-min+1))+min)

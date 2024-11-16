fc398fde3fbfc4107f566bab563cea4dc1895b1b
aaref12 <111276585+aaref12@users.noreply.github.com>
2024-11-16 07:17:41 +0000

    striing

---
+++ .devcontainer/01_basic/string.js
@@ -0,0 +1,14 @@
+const gameName=new String("coderNinga")
+const paragraph = "I think Ruth's dog is cuter than your dog!";
+const search="i"
+const manuftureDate=2023
+
+const name="   aaref     "
+console.log(`this game name is ${gameName} and manufactre date is ${manuftureDate} `)
+console.log(gameName.charAt(0))
+console.log(gameName.indexOf("d"))
+console.log(gameName.includes("z"))
+console.log(paragraph.lastIndexOf(search))
+console.log(paragraph.substring(3,5))
+console.log(name)
+console.log(name.trim())

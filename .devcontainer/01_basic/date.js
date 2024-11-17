079e7c614d3a487a206d22067b4a028dce0d94a5
aaref12 <111276585+aaref12@users.noreply.github.com>
2024-11-17 05:36:00 +0000

    date

---
+++ .devcontainer/01_basic/date.js
@@ -0,0 +1,7 @@
+const date=new Date();
+console.log(date)
+console.log(date.toLocaleString())
+ console.log(date.toDateString())   
+ console.log(date.toISOString()) 
+ console.log(date.toJSON()) 
+ console.log(`date in json ${date.toLocaleTimeString()}`) 

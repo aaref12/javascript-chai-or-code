a15bd2b87d82925d54a3aacd8dbf884eba7c4117
aaref12 <111276585+aaref12@users.noreply.github.com>
2024-11-18 06:25:48 +0000

    object

---
+++ .devcontainer/02_basic/01_object.js
@@ -0,0 +1,8 @@
+const userName={
+    name:"shaikh aaref hamza",
+    age:23,
+    email:"shaikhaaref@google.com",
+    address:"qudwai Nagar Degloor Naka Nanded 431604",
+}
+
+console.log(userName["name","age"])
